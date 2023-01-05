eaea309a5bf2f41c1cf114e4015c3cea23a272a9
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2023-01-05 06:51:01 +0100

    add dashboard

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,12 +16,14 @@ export class RegisterComponent {
     password: ['', [Validators.required, Validators.minLength(6)]]
   })
 
-  constructor(private formbuilder: FormBuilder) { }
+  constructor(private formbuilder: FormBuilder,
+    private router: Router) { }
 
   registro() {
     console.log(this.miFormulario.valid);
-    console.log(this.miFormulario.value);
+    // console.log(this.miFormulario.value);
 
+    this.router.navigateByUrl('/dashboard');
   }
 
 }
